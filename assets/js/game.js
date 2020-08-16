a7a0aaaf24f74623d67fadcf4563494bb7d5d514
Daniel Gross <danwgross@gmail.com>
2020-08-16 15:59:18 -0400

    3.1.6 manage data with functions variables and operators

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,26 @@
+const playerName = window.prompt("What is your robot's name?");
+let playerHealth = 100;
+let playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+const enemyName = "Roborto";
+let enemyHealth = 50;
+let enemyAttack = 12;
+
 const fight = () => {
-    window.alert("The fight has begun!")
+    // Alert users that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth -= playerAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health remaining.`);
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth -= enemyAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining.`);
 };
 
-const playerName = window.prompt(`What is your robot's name`);
-console.log(playerName)
-
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log(`Our robot's name is ${playerName}`);
-// fight();
+fight();
